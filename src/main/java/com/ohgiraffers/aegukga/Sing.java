5e6ad443862d0d6ec65bad774321503009d0545d
triglan <121035322+triglan@users.noreply.github.com>
2026-08-02 16:29:14 +0900

    git 마르고 닳도록 충돌 해결

--- src/main/java/com/ohgiraffers/aegukga/Sing.java
+++ src/main/java/com/ohgiraffers/aegukga/Sing.java
@@ -8,7 +8,7 @@ public class Sing {
         System.out.println("동해물과 백두산이");
         System.out.println("마르고 닳도록");
         System.out.println("하느님이 보우하사");
-        
+
         // 후렴구 출력부
         System.out.println("무궁화 삼천리 화려강산" +
                 "대한사람 대한으로 길이 보전하세");
