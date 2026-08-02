7079a88203e0f7cbb1e826d91f3a2bf4770ea1f5
triglan <121035322+triglan@users.noreply.github.com>
2026-08-02 13:41:36 +0900

    1절 가사 추가 (마르고 닳도록)

--- src/main/java/com/ohgiraffers/aegukga/Sing.java
+++ src/main/java/com/ohgiraffers/aegukga/Sing.java
@@ -7,7 +7,9 @@ public class Sing {
         // 1절 출력부
 
         // 후렴구 출력부
-        System.out.println("무궁화 삼천리 화려강산" +
+        System.out.println(
+                "마르고 닳도록" +
+                "무궁화 삼천리 화려강산" +
                 "대한사람 대한으로 길이 보전하세");
     }
 }
