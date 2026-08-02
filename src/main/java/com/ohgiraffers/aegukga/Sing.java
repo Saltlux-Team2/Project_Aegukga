979c95b15c2a88f0584ad43e5511a7fdcf48d9cc
triglan <121035322+triglan@users.noreply.github.com>
2026-08-02 13:48:51 +0900

    마르고 닳도록 1차 수정

--- src/main/java/com/ohgiraffers/aegukga/Sing.java
+++ src/main/java/com/ohgiraffers/aegukga/Sing.java
@@ -5,11 +5,9 @@ public class Sing {
     public static void main(String[] args) {
 
         // 1절 출력부
-
+        System.out.println("마르고 닳도록");
         // 후렴구 출력부
-        System.out.println(
-                "마르고 닳도록" +
-                "무궁화 삼천리 화려강산" +
+        System.out.println("무궁화 삼천리 화려강산" +
                 "대한사람 대한으로 길이 보전하세");
     }
 }
